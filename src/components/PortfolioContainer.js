bb0ac209df9df57c2f2cbe9022403f4a1403bdd0
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-08 16:35:49 -0500

    set up base layer

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import NavTabs from './NavTabs';
+import Bio from './pages/Bio';
+import Apps from './pages/Apps';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Bio');
+
+  const renderPage = () => {
+
+    if (currentPage === 'Bio') {
+      return <Bio />;
+    }
+    if (currentPage === 'Apps') {
+      return <Apps />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
