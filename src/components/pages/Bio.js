dad069b793eae996876d8297e0afea277b35bc82
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-09 16:45:52 -0500

    dev'd bio

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -4,26 +4,38 @@ export default function Bio() {
   return (
     <div className="bio-container">
       <div className="card-group">
-        <div className="card">
-          <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="card">
+          <h3 class="card-header" id="interests-header">Interests</h3>
+          <div class="card-body">
+            <h4 class="card-title">App development</h4>
+            <p class="card-text">entertainment</p>
+            <p class="card-text">music</p>
+            <p class="card-text">niche necessities</p>
+            <h4 class="card-title">Web design</h4>
+            <p class="card-text">food/textiles</p>
+            <p class="card-text">e-commerce</p>
+            <p class="card-text">renewable energy</p>
           </div>
         </div>
         <div className="card" id="img-card">
           <img className="card-img-top" src="/assets/images/avatar.png" alt="Avatar Icon" height="300" />
           <div className="card-body">
-            <h4 className="card-title">Card title</h4>
+            <h4 className="card-title" id="status-title">Status</h4>
             <p className="card-text">
-              Some quick example text to build on the card title
-              and make up the bulk of the card's content.
+              UMN coding student developer
             </p>
           </div>
         </div>
-        <div className="card">
-          <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <div class="card">
+          <h3 class="card-header" id="hobbies-header">Hobbies</h3>
+          <div class="card-body">
+            <ul class="list-group" id="hobbies-body">
+              <li class="list-group-item">Running</li>
+              <li class="list-group-item" id="even-hobbies">Coding</li>
+              <li class="list-group-item">Gaming</li>
+              <li class="list-group-item" id="even-hobbies">Music</li>
+              <li class="list-group-item">Anime</li>
+            </ul>
           </div>
         </div>
       </div>
