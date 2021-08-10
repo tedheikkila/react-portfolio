1da3abde92f39b348636cdcbf4859ad675c07b97
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-09 19:05:24 -0500

    styling bio, apps

--- src/components/pages/Apps.js
+++ src/components/pages/Apps.js
@@ -3,88 +3,88 @@ export default function Apps() {
   return (
     <section class="apps justify-content-center">
       <div class="semantics">
-        <a href=" https://tedheikkila.github.io/semantic-tags" target="_blank">
+        <a href=" https://tedheikkila.github.io/semantic-tags" target="_blank" rel="noreferrer">
           <img src="./assets/images/semantic-tags.png" width="320" height="270" alt="Semantic Tags" />
         </a>
-        <p><a href="https://github.com/tedheikkila/semantic-tags" target="_blank">Semantic Tags - HTML/CSS</a></p>
+        <p><a href="https://github.com/tedheikkila/semantic-tags" target="_blank" rel="noreferrer">Semantic Tags - HTML/CSS</a></p>
       </div>
       <div class="password-gen">
-        <a href="https://tedheikkila.github.io/password-generator" target="_blank">
+        <a href="https://tedheikkila.github.io/password-generator" target="_blank" rel="noreferrer">
           <img src="./assets/images/password.png" width="320" height="270" alt="Password Generator" />
         </a>
-        <p><a href="https://github.com/tedheikkila/password-generator" target="_blank">Password Generator - Js</a></p>
+        <p><a href="https://github.com/tedheikkila/password-generator" target="_blank" rel="noreferrer">Password Generator - Js</a></p>
       </div>
       <div class="coding-quiz">
-        <a href="https://tedheikkila.github.io/coding-quiz" target="_blank">
+        <a href="https://tedheikkila.github.io/coding-quiz" target="_blank" rel="noreferrer">
           <img src="./assets/images/quiz.png" width="320" height="270" alt="Coding Quiz" />
         </a>
-        <p><a href="https://github.com/tedheikkila/coding-quiz" target="_blank">Coding Quiz - HTML/CSS/Js</a></p>
+        <p><a href="https://github.com/tedheikkila/coding-quiz" target="_blank" rel="noreferrer">Coding Quiz - HTML/CSS/Js</a></p>
       </div>
       <div class="daily-planner">
-        <a href="https://tedheikkila.github.io/daily-planner" target="_blank">
+        <a href="https://tedheikkila.github.io/daily-planner" target="_blank" rel="noreferrer">
           <img src="./assets/images/planner.png" width="320" height="270" alt="Daily Planner" />
         </a>
-        <p><a href="https://github.com/tedheikkila/daily-planner" target="_blank">Daily Planner - Moment/jQ</a></p>
+        <p><a href="https://github.com/tedheikkila/daily-planner" target="_blank" rel="noreferrer">Daily Planner - Moment/jQ</a></p>
       </div>
       <div class="weather-dashboard">
-        <a href="https://tedheikkila.github.io/weather-dashboard" target="_blank">
+        <a href="https://tedheikkila.github.io/weather-dashboard" target="_blank" rel="noreferrer">
           <img src="./assets/images/weather.png" width="320" height="270" alt="Weather Dashboard" />
         </a>
-        <p><a href="https://github.com/tedheikkila/weather-dashboard" target="_blank">Weather Dashboard - API/Js</a></p>
+        <p><a href="https://github.com/tedheikkila/weather-dashboard" target="_blank" rel="noreferrer">Weather Dashboard - API/Js</a></p>
       </div>
       <div class="movie-pop">
-        <a href="https://tedheikkila.github.io/movie-pop" target="_blank">
+        <a href="https://tedheikkila.github.io/movie-pop" target="_blank" rel="noreferrer">
           <img src="./assets/images/movie.png" width="320" height="270" alt="Movie Pop" />
         </a>
-        <p><a href="https://github.com/tedheikkila/movie-pop" target="_blank">Movie Pop - B/Js/API</a></p>
+        <p><a href="https://github.com/tedheikkila/movie-pop" target="_blank" rel="noreferrer">Movie Pop - B/Js/API</a></p>
       </div>
       <div class="readme-generator">
-        <a href="https://tedheikkila.github.io/readme-generator" target="_blank">
+        <a href="https://tedheikkila.github.io/readme-generator" target="_blank" rel="noreferrer">
           <img src="./assets/images/readme-gen.png" width="320" height="270" alt="Readme Generator" />
         </a>
-        <p><a href="https://github.com/tedheikkila/readme-generator" target="_blank">Readme Generator - Node/Md</a></p>
+        <p><a href="https://github.com/tedheikkila/readme-generator" target="_blank" rel="noreferrer">Readme Generator - Node/Md</a></p>
       </div>
       <div class="team-generator">
-        <a href="https://github.com/tedheikkila/team-generator" target="_blank">
+        <a href="https://github.com/tedheikkila/team-generator" target="_blank" rel="noreferrer">
           <img src="./assets/images/team-gen.png" width="320" height="270" alt="Team Generator" />
         </a>
-        <p><a href="https://github.com/tedheikkila/team-generator" target="_blank">Team Generator - Jest/Inq</a></p>
+        <p><a href="https://github.com/tedheikkila/team-generator" target="_blank" rel="noreferrer">Team Generator - Jest/Inq</a></p>
       </div>
       <div class="note">
-        <a href="https://fatidique-saucisson-41554.herokuapp.com/" target="_blank">
+        <a href="https://fatidique-saucisson-41554.herokuapp.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/note.png" width="320" height="270" alt="Note Taker" />
         </a>
-        <p><a href="https://github.com/tedheikkila/note-taker" target="_blank">Note Taker - Express/Heroku</a></p>
+        <p><a href="https://github.com/tedheikkila/note-taker" target="_blank" rel="noreferrer">Note Taker - Express/Heroku</a></p>
       </div>
       <div class="employee">
-        <a href="https://github.com/tedheikkila/employee-tracker" target="_blank">
+        <a href="https://github.com/tedheikkila/employee-tracker" target="_blank" rel="noreferrer">
           <img src="./assets/images/employee.png" width="320" height="270" alt="Employee Tracker" />
         </a>
-        <p><a href="https://github.com/tedheikkila/employee-tracker" target="_blank">Employee Tracker - MySQL/Inq</a></p>
+        <p><a href="https://github.com/tedheikkila/employee-tracker" target="_blank" rel="noreferrer">Employee Tracker - MySQL/Inq</a></p>
       </div>
       <div class="e-commerce">
-        <a href="https://github.com/tedheikkila/e-commerce" target="_blank">
+        <a href="https://github.com/tedheikkila/e-commerce" target="_blank" rel="noreferrer">
           <img class="e-commerce" src="./assets/images/e-commerce.png" width="320" height="270" alt="E-Commerce" />
         </a>
-        <p><a href="https://github.com/tedheikkila/e-commerce" target="_blank">E-Commerce - MySQL/Postman</a></p>
+        <p><a href="https://github.com/tedheikkila/e-commerce" target="_blank" rel="noreferrer">E-Commerce - MySQL/Postman</a></p>
       </div>
       <div class="tech-blog">
-        <a href="https://aqueous-taiga-82354.herokuapp.com/" target="_blank">
+        <a href="https://aqueous-taiga-82354.herokuapp.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/tech-blog.png" width="320" height="270" alt="Tech Blog" />
         </a>
-        <p><a href="https://github.com/tedheikkila/tech-blog" target="_blank">Tech Blog - MVC/Sqlize</a></p>
+        <p><a href="https://github.com/tedheikkila/tech-blog" target="_blank" rel="noreferrer">Tech Blog - MVC/Sqlize</a></p>
       </div>
       <div class="one-track">
-        <a href="https://arcane-castle-03954.herokuapp.com/" target="_blank">
+        <a href="https://arcane-castle-03954.herokuapp.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/one-track.png" width="320" height="270" alt="One Track" />
         </a>
-        <p><a href="https://github.com/tedheikkila/one-track" target="_blank">One Track - MVC/Handlebars</a></p>
+        <p><a href="https://github.com/tedheikkila/one-track" target="_blank" rel="noreferrer">One Track - MVC/Handlebars</a></p>
       </div>
       <div class="workout-tracker">
-        <a href="https://enigmatic-ravine-88709.herokuapp.com/" target="_blank">
+        <a href="https://enigmatic-ravine-88709.herokuapp.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/workout-tracker.png" width="320" height="270" alt="Workout Tracker" />
         </a>
-        <p><a href="https://github.com/tedheikkila/workout-tracker" target="_blank">Workout Tracker - Mongo/Express</a></p>
+        <p><a href="https://github.com/tedheikkila/workout-tracker" target="_blank" rel="noreferrer">Workout Tracker - Mongo/Express</a></p>
       </div>
     </section>
   );
--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -39,7 +39,6 @@ export default function Bio() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
