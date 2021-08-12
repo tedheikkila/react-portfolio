063a9d0181c2bdd852f63fb97708f51002f0193e
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-11 20:18:09 -0500

    updated contact form w/bootstrap

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -27,13 +27,16 @@ export default function Contact() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    if (!validateEmail(email) || !name) {
-      setErrorMessage('Email is invalid');
+    if (!validateEmail(email)) {
+      setErrorMessage('Please enter a valid email');
       return;
+    } else if (!name) {
+      setErrorMessage('Please provide your name')
+      return
+    } else if (!message) {
+      setErrorMessage('Please type a message')
+      return
     }
-   
-    alert(`Hello ${name}`);
-
     setName('');
     setEmail('');
     setMessage('');
@@ -41,32 +44,57 @@ export default function Contact() {
 
   return (
     <div>
-      <p>Hello {name}</p>
+      <h3>Hello {name}</h3>
       <form className="form">
-       
-        <input
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-         <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email@mail.com"
-        />
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="message"
-          placeholder="Type message here"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
+        <div className="form-group row">
+          <label for="input-name" className="col-sm-2 col-form-label">Name</label>
+          <div className="col-sm-10">
+            <input
+              value={name}
+              name="name"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Full Name"
+              id="input-name"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label for="input-email" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              id="input-email"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="email@mail.com"
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label for="input-message" class="col-sm-2 col-form-label">Message</label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="input-message"
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              placeholder="Type question or comment here"
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <div className="col-sm-10 offset-sm-2">
+            <button type="button" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+          </div>
+        </div>
       </form>
+
       {errorMessage && (
         <div>
           <p className="error-text">{errorMessage}</p>
