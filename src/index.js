ac5821d70bf47f675d2bc19b21b4b3f36ffaf68f
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-25 17:49:12 -0500

    changed favicon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Favicon from 'react-favicon'
 
 // renders to index.html root
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<React.StrictMode>
+    <Favicon url='https://github.githubassets.com/favicons/favicon.png' />
+    <App />
+</React.StrictMode>,
+document.getElementById('root'));
